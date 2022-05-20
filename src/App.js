3c1aef0e45e5f4c6f33c502619b81209b3a8faef
MD ABDULLAH ALL SAJAL <abdullah.sajal@tuni.fi>
2022-05-20 19:01:56 +0300

    AppBar and Menu added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import VerticalTabs from './components/VerticalTabs';
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar />
+      <VerticalTabs />
+    </div>
+  );
+}
+
+export default App;
